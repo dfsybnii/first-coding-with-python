c02ec4d6072a3d3b5a1e08b2691a5ee20eabd730
dfsybnii <muhammad.daffa.syabaniii@gmail.com>
2025-02-27 13:07:44 +0700

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,25 @@
+
+
+# Ask user to user for hello #
+name = input( "What's your name?" ).strip().title().capitalize().title().strip()
+
+#split name user 
+first, last = name.split(" ")
+
+
+# Say hello to  user #
+print(f" halo, {first}")
+
+
+# Small calculator
+
+x = int (input("whats x ? "))
+y = int (input("whats y ? "))
+
+
+print(x + y)
+
+
+  
+    
+  
