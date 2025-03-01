79cc78f213167827e7464c44ca0de454be8756ac
dfsybnii <muhammad.daffa.syabaniii@gmail.com>
2025-03-01 15:40:14 +0700

    Create Conditional.py

---
+++ Conditional.py
@@ -0,0 +1,13 @@
+
+Dia_suka_ama_gua = False #judul pertanyaan
+
+
+jawaban = input (" Apakah dia suka ama lu? (yes/no)").lower()
+
+if jawaban == "yes":
+
+    print(" Ga, dia ga suka ama lu ")
+
+True == "no"
+     
+print(" Ga, Dia ga suka ama lu.")
